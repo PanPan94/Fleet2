45b42f3df456b4bc150fd9c80c6d31af2f09f095
PanPan94 <paartheepan94@gmail.com>
2018-01-16 13:37:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet"> 
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="global" id="global">
+        <div class="menu-toggler" id="menu-toggler"></div>
+    <!-- SIDEBAR -->
+        <div class="menu" id="menu">
+            <div class="menu-header">
+                <div class="profile-pic">
+                    <img src="./img/user.png" alt="user">
+                </div>
+                <div class="profile-name">
+                    <h2>San<span class="fname">Kaÿzn</span></h2>
+                </div>
+            </div>
+
+            <div class="menu-items">
+                <div class="menu-item">
+                    <i class="menu-item-logo-profile"></i> <a href="#">Voir mon profil</a>
+                </div>
+                <div class="menu-item">
+                    <i class="menu-item-logo-fav"></i> <a href="#">Voir mon profil</a>
+                </div>
+                <div class="menu-item">
+                    <i class="menu-item-logo-help"></i> <a href="#">Voir mon profil</a>
+                </div>
+            </div>
+
+            <div class="menu-btns">
+                <div class="menu-btn"><a href="#">Web</a></div>
+                <div class="menu-btn"><a href="#">Opinions</a></div>
+                <div class="menu-btn"><a href="#">Start-ups</a></div>
+                <div class="menu-btn"><a href="#">LifeStyle</a></div>
+            </div>
+        </div>
+    <!-- NAVBAR -->
+        <header class="header">
+            <div class="nav">
+                <div class="nav-items"><a href="#">Inscription</a></div>
+                <div class="nav-items"><a href="#">Connexion</a></div>
+            </div>
+        </header>
+    <!-- SEARCHBAR -->
+        <div class="main-caption">
+            <div class="form-field">
+                <div class="field-caption">
+                    <h3>Fleet</h3>
+                    <h2>Vos idées aux yeux du monde entier.</h2>
+                </div>
+                <div class="field">
+                    <input type="text" class="field-input" placeholder="Explorer les projets">
+                    <input type="submit" value="Chercher" class="field-search">
+                </div>
+            </div>
+        </div>
+    <!-- LATEST POSTS -->
+        <div class="h--posts">
+            <div class="h--posts-item">
+                <a class="h--post-link" href="#"></a>
+                <div class="h--post-item-img">
+                    <img src="https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" alt="project_image">
+                </div>
+                <div class="h--post-desc">
+                    <h2>Wylido</h2>
+                    <p>Site de rencontre pour chiens</p>
+                    <span class="h--post-subdesc">
+                        Cherche un chien.
+                    </span>
+                </div>
+            </div>
+            
+            <div class="h--posts-item">
+                <a class="h--post-link" href="#"></a>
+                <div class="h--post-item-img">
+                    <img src="https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" alt="project_image">
+                </div>
+                <div class="h--post-desc">
+                    <h2>Wylido</h2>
+                    <p>Site de rencontre pour chiens</p>
+                </div>
+            </div>
+            
+            <div class="h--posts-item">
+                <div class="h--post-item-img">
+                    <img src="https://images.unsplash.com/photo-1502673530728-f79b4cab31b1?dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb" alt="project_image">
+                </div>
+                <div class="h--post-desc">
+                    <h2>Wylido</h2>
+                    <p>Site de rencontre pour chiens</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- SCRIPTS -->
+    <script src="js/app.js"></script>
+</body>
+</html>
